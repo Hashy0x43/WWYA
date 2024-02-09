1db7757b158b2a465b17b4b229cdab549ebfdcb3
hashy <hashy@tutanota.de>
2024-02-09 00:39:47 -0500

    Bugger Off
    Fixed the localhost & index.html issues.
    
    Don't expect much from index.html though. I just whipped one up to get one out. I'll see if I can add more & pretty it up later.

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 
 from flask import Flask, request, render_template, url_for
 from requests import get
+from sys import argv
 
 
 server = Flask(__name__)
@@ -9,9 +10,7 @@ server = Flask(__name__)
 @server.route("/")
 def index():
     client = request.remote_addr
-
-
-    if client.startswith(("127.", "10.", "198.168.", "172.16.", "172.31.")):
+    if client.startswith(("192.164.", "10.", "100.", "172.", "192.0.", "198.18.")):
         you = get("https://4.ident.me").text
         ypayload = {"ip" : you};del you
         location = get("http://ip-api.com/json/", params = ypayload).json();del ypayload
@@ -19,29 +18,27 @@ def index():
         cpayload = {"ip" : client};del client
         location = get("http://ip-api.com/json/", params = cpayload).json();del cpayload
 
-
     usesFahr = ["US", "AS", "KY", "GU", "LR", "MH", "FM", "MP", "PW", "PR", "VI"]
     if location["countryCode"] in usesFahr:
         unitsDesired = "imperial"
-        unitsShort = 'F'
+        shortUnit = 'F'
     else:
         unitsDesired = "metric"
-        unitsShort = 'C'
+        shortUnit = 'C'
     del usesFahr
 
-
     wpayload = {
-                    "appid" : "abfb1f37fab3682c21047e660f38329d", # API Key from OpenWeatherMap
+                    "appid" : argv[1], # API Key from OpenWeatherMap
                     "lat" : location['lat'], "lon" : location['lon'],
                     "units": unitsDesired
             }
     weather = get("https://api.openweathermap.org/data/2.5/weather", params = wpayload).json();del wpayload
 
 
-    return render_template("index.html", location = location, weather = weather, unitsShort = unitsShort)
+    return render_template("index.html", location = location, weather = weather, shortUnit = shortUnit)
 
 
 if __name__ == "__main__":
-    server.run("127.0.0.1", 1984)
+    server.run("0.0.0.0", 1984)
 
 
